2a0dc20f47ee590de883dbcf2f2737879cb89aa2
Blair Trump <blair@blairtrump.com>
2013-07-31 15:32:37 -0400

    Changes to cron timing

--- twitter/cron.py
+++ twitter/cron.py
@@ -2,14 +2,15 @@ from django_cron import CronJobBase, Schedule
 from twitter.models import Application
 
 class ApplicationSearch(CronJobBase):
-  RUN_EVERY_MINS = 0.1  # every 2 hoursr
+  RUN_EVERY_MINS = 15
 
   schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
   code = 'twitter.application_search'  # a unique code
 
   def do(self):
     applications = Application.objects.all()
-    responses = []
     for application in applications:
-      responses.append(application.search_all_terms())
-    print responses
+      message = "cron job triggered for application '%s'" % application
+      print message
+      application.logger.info(message)
+      application.search_all_terms()
