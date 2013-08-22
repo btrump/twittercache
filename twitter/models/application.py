8049669a258643b40069f09cb54698703908f77e
Blair Trump <blair@blairtrump.com>
2013-08-22 13:49:15 -0400

    Refactored info into dicts.  Recording search stats.

--- twitter/models/application.py
+++ twitter/models/application.py
@@ -67,40 +67,71 @@ class Application(models.Model):
   def search_all_terms(self):
     self.logger.debug('Searching all terms (%s): %s' % (self.searchterm_set.count(), self.search_terms()))
 
-    response = []
+    info = {
+            'total_result_count': 0,
+            'total_results_duplicate_count': 0,
+            'total_results_error_count': 0,
+            'total_results_saved_count': 0,
+            'total_payload_size': 0,
+            'terms_searched': 0,
+            }
+    term_results = {}
     for term in self.searchterm_set.all():
       term_requests = int((term.weight / float(self.get_search_weights_sum())) * self.rate_limit_requests)
+      info['terms_searched'] += 1
       self.logger.debug('Giving %s a total of %s requests ((%s weight / %s weight sum) * %s rate limit)' % (term, term_requests, term.weight, self.get_search_weights_sum(), self.rate_limit_requests))
       for i in range(term_requests):
-        response.append(self.search_term(term))
-    return response
+        term_result = self.search_term(term)
+        info['total_result_count'] += term_result['info']['result_count']
+        info['total_results_duplicate_count'] += term_result['info']['results_duplicate_count']
+        info['total_results_error_count'] += term_result['info']['results_error_count']
+        info['total_results_saved_count'] += term_result['info']['results_saved_count']
+        info['total_payload_size'] += term_result['info']['payload_size']
+        term_results.update({unicode(term):term_result})
+    return {'terms':term_results, 'info':info}
   
   def search_term(self, term):
     from twitter.models import Tweet
     import json
 
     try:
-      search_params={'q':term, 'count':self.results_per_request}
+      search_params = {'q':unicode(term), 'count':self.results_per_request}
       self.logger.debug('Searching with parameters %s' % search_params)
       search_service_path = "search/tweets.json"
       session = self.get_session()
       response = session.get(search_service_path, params=search_params).json()
       statuses = response['statuses']
-      self.logger.debug('Got %s results' % statuses.__len__())
-      term.increment_search_counter()
-      self.store_payload(response)
+      info = {
+              'count': self.results_per_request,
+              'result_count': statuses.__len__(),
+              'search_counter': term.increment_search_counter(),
+              'results_saved_count': 0,
+              'results_duplicate_count': 0,
+              'results_error_count': 0,
+              'payload_size': json.dumps(response).__len__(),
+              'payload_path': self.store_payload(json.dumps(response)),
+              }
+
+      self.logger.debug('Got %s results' % info['result_count'])
   
       for result in statuses:
-        tweet = Tweet.create(result, term)
-        tweet.user.save()
-        try:
-          tweet.save()
-        except Exception as e:
-          logged_tweet_path = tweet.log()
-          self.logger.error("Could not save tweet with id %s to database.  %s.  JSON response logged to %s" % (tweet.id, e, logged_tweet_path))
-      return statuses
+        tweet = Tweet.objects.get_or_none(pk=result['id'])
+        if tweet:
+          info['results_duplicate_count'] += 1
+        else:
+          try:
+            tweet = Tweet.create(result, term)
+            tweet.user.save()
+            tweet.save()
+            info['results_saved_count'] += 1
+          except Exception as e:
+            info['results_error_count'] += 1
+            logged_tweet_path = tweet.log()
+            self.logger.error("Could not save tweet with id %s to database.  %s.  JSON response logged to %s" % (tweet.id, e, logged_tweet_path))
+      return {'info':info, 'statuses':statuses}
     except Exception as e:
       self.logger.error(e)
+      return None
 
   def get_session(self):
     from rauth import OAuth1Service
@@ -131,6 +162,7 @@ class Application(models.Model):
     now = datetime.now()
     log_path = "./twittercache/logs/payloads/%s/" % now.strftime('%Y/%m/%d') 
     filename = now.strftime('%H%M%S.%f.json')
+    path_to_log = log_path + filename
     try:
       os.makedirs(log_path)
     except OSError as e:
@@ -138,6 +170,7 @@ class Application(models.Model):
         pass
       else:
         raise
-    with open(log_path + filename, 'w') as f:
+    with open(path_to_log, 'w') as f:
       print(payload, file=f)
     f.close
+    return path_to_log
